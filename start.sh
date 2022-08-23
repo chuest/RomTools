cd4afb483682313397150e7076b148ae33e92a1a
chuest <2474681296@qq.com>
2022-08-23 20:01:13 +0800

    update

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 function main(){
 	romName=${1}
-	rootPath=`pwd`	#项目脚本
+	rootPath=`pwd`
 	mkdir out
 	echo "正在解压刷机包"
 	unzip -o $romName -d out
@@ -13,16 +13,14 @@ function main(){
 	${rootPath}/bin/payload-dumper-go -o ${rootPath}/out payload.bin > /dev/null
 	rm -rf payload.bin
 	unpackimg system
-	#unpackimg vendor
-	#unpackimg product
-	#unpackimg system_ext
+	unpackimg vendor
+	unpackimg product
 	modify
 	repackimg system
-	#repackimg vendor
-	#repackimg product
-	#repackimg system_ext
-	# super
-	sudo rm -rf _pycache_ system vendor product system_ext # system.img vendor.img product.img system_ext.img
+	repackimg vendor
+	repackimg product
+	super
+	sudo rm -rf _pycache_ system vendor product system.img vendor.img product.img system_ext.img odm.img
 	sudo zip -q -r rom.zip *.img
 	sudo rm -rf *.img
 }
@@ -36,7 +34,6 @@ function unpackimg(){
 function repackimg(){
 	name=${1}
 	mount_path="/$name"
-	# if [ "$name" == "system" ];then mount_path="/" ;else mount_path="/$name" ;fi
 	fileContexts="${rootPath}/out/${name}/config/${name}_file_contexts"
 	fsConfig="${rootPath}/out/${name}/config/${name}_fs_config"
 	imgSize=`echo "$(sudo du -sb ${rootPath}/out/${name} | awk {'print $1'}) + 104857600" | bc`
@@ -47,154 +44,152 @@ function repackimg(){
 }
 
 function super(){
-	# $(echo $(stat -c "%s" system.img)+$(stat -c "%s" vendor.img)+$(stat -c "%s" system_ext.img)+$(stat -c "%s" product.img)+$(stat -c "%s" odm.img) | bc)
 	${rootPath}/bin/lpmake --metadata-size 65536 --super-name super --device super:9126805504 --group main_a:9126805504 --group main_b:9126805504 --metadata-slots 3 --virtual-ab --partition system_a:readonly:$(echo $(stat -c "%s" system.img) | bc):main_a --image system_a=system.img --partition system_b:readonly:0:main_b --partition vendor_a:readonly:$(echo $(stat -c "%s" vendor.img) | bc):main_a --image vendor_a=vendor.img --partition vendor_b:readonly:0:main_b --partition product_a:readonly:$(echo $(stat -c "%s" product.img) | bc):main_a --image product_a=product.img --partition product_b:readonly:0:main_b --partition system_ext_a:readonly:$(echo $(stat -c "%s" system_ext.img) | bc):main_a --image system_ext_a=system_ext.img --partition system_ext_b:readonly:0:main_b --partition odm_a:readonly:$(echo $(stat -c "%s" odm.img) | bc):main_a --image odm_a=odm.img --partition odm_b:readonly:0:main_b --sparse --output super.img
 }
 
 function modify(){
+	# replace vbmeta images
+	echo "正在去除vbmeta验证"
+	[ -f "$targetVbmetaImage" ] && cp -rf ${rootPath}/files/vbmeta.img ${rootPath}/out/vbmeta.img
+	[ -f "$targetVbmetaSystemImage" ] && cp -rf ${rootPath}/files/vbmeta_system.img ${rootPath}/out/vbmeta_system.img
+	# [ -f "$targetVbmetaVendorImage" ] && cp -rf ${rootPath}/files/vbmeta_vendor.img ${rootPath}/out/vbmeta_vendor.img
+
+	# sed -i 's/\x00\x00\x00\x00\x00\x61\x76\x62\x74\x6F\x6F\x6C\x20\x31\x2E\x31\x2E\x30/\x02\x00\x00\x00\x00\x61\x76\x62\x74\x6F\x6F\x6C\x20\x31\x2E\x31\x2E\x30/g' ${rootPath}/files/vbmeta.img
+
+	# remove avb parttn
+	cp ${rootPath}/files/Magisk.apk Magisk.zip
+	unzip -o Magisk.zip -d Magisk
+	sudo mv Magisk/lib/x86/libmagiskboot.so ${rootPath}/bin/magiskboot
+	echo "正在使用magisk修补boot"
+	${rootPath}/bin/magiskboot unpack boot.img >/dev/null 2>&1
+	${rootPath}/bin/magiskboot cpio ramdisk.cpio patch
+	for dt in dtb kernel_dtb extra; do
+		[ -f $dt ] && ${rootPath}/bin/magiskboot dtb $dt patch
+	done
+	${rootPath}/bin/magiskboot repack boot.img >/dev/null 2>&1
+	rm -rf *kernel* *dtb* ramdisk.cpio*
+	[ -f new-boot.img ] && cp -rf new-boot.img boot.img
+	rm -rf new-boot.img
+
+	echo "正在使用magisk修补vendor_boot"
+	${rootPath}/bin/magiskboot unpack vendor_boot.img >/dev/null 2>&1
+	${rootPath}/bin/magiskboot cpio ramdisk.cpio patch
+	for dt in dtb kernel_dtb extra; do
+        [ -f $dt ] && ${rootPath}/bin/magiskboot dtb $dt patch
+	done
+	${rootPath}/bin/magiskboot repack vendor_boot.img >/dev/null 2>&1
+	rm -rf *kernel* *dtb* ramdisk.cpio*
+	[ -f new-boot.img ] && cp -rf new-boot.img vendor_boot.img
+	rm -rf new-boot.img
+
+	# buil twrp recovery for vab devices
+	#${rootPath}/bin/magiskboot unpack boot.img >/dev/null 2>&1
+	#echo "正在刷入TWRP"
+	#cp -rf ${rootPath}/files/ramdisk.cpio ramdisk.cpio
+	#${rootPath}/bin/magiskboot repack boot.img >/dev/null 2>&1
+	#mv new-boot.img boot.img
+	#bash bin/Universal/Magisk/boot_patch.sh boot.img
+	#rm boot.img
+	#cp -rf new-boot.img tmp/boot_twrp.img
+	#rm -rf *kernel* *dtb* ramdisk.cpio* new-boot.img
+
 	# system
 	sudo sed -i '0,/[a-z]\+\/lost\\+found/{/[a-z]\+\/lost\\+found/d}' system/config/system_file_contexts
 
+	cat ${rootPath}/files/system_file_contexts_add.txt >>system/config/system_file_contexts
+	cat ${rootPath}/files/system_fs_config_add.txt >>system/config/system_fs_config
+
+	sudo rm -rf system/system/verity_key
+	sudo rm -rf system/system/system/media/theme/miui_mod_icons/com.google.android.apps.nbu
+	sudo rm -rf system/system/system/media/theme/miui_mod_icons/dynamic/com.google.android.apps.nbu
 	# Analytics
-	sudo rm -rf system/system/system/app/AnalyticsCore
-	#sudo sed -i '/app\/AnalyticsCore/d' system/config/system_file_contexts
-	#sudo sed -i '/app\/AnalyticsCore/d' system/config/system_fs_config
+	sudo rm -rf system/system/system/app/AnalyticsCore/*
+	sudo cp ${rootPath}/files/AnalyticsCore.apk system/system/system/app/AnalyticsCore/AnalyticsCore.apk
+	# 酷安
+	sudo mkdir system/system/system/data-app/Coolapk
+	sudo cp ${rootPath}/files/Coolapk.apk system/system/system/data-app/Coolapk/Coolapk.apk
+	# MT管理器
+	sudo mkdir system/system/system/data-app/MTManager
+	sudo cp ${rootPath}/files/MTManager.apk system/system/system/data-app/MTManager/MTManager.apk
+	# Magisk
+	sudo mkdir system/system/system/data-app/Magisk
+	sudo cp ${rootPath}/files/Magisk.apk system/system/system/data-app/Magisk/Magisk.apk
+	#
+	sudo mv system/system/system/app/Updater/Updater.apk system/system/system/app/Updater/Updater.apk.bak
+	#
+	sudo mv system/system/system/app/PowerKeeper/PowerKeeper.apk system/system/system/app/PowerKeeper/PowerKeeper.apk.bak
 	# 电商助手
 	sudo rm -rf system/system/system/app/mab
-	#sudo sed -i '/app\/mab/d' system/config/system_file_contexts
-	#sudo sed -i '/app\/mab/d' system/config/system_fs_config
 	#
 	sudo rm -rf system/system/system/app/MiuiBugReport
-	#sudo sed -i '/app\/MiuiBugReport/d' system/config/system_file_contexts
-	#sudo sed -i '/app\/MiuiBugReport/d' system/config/system_fs_config
-	#
+	# 应用商店
 	sudo rm -rf system/system/system/app/MIUISuperMarket
-	#sudo sed -i '/app\MIUISuperMarket/d' system/config/system_file_contexts
-	#sudo sed -i '/app\/MIUISuperMarket/d' system/config/system_fs_config
 	#
 	sudo rm -rf system/system/system/app/MSA
-	#sudo sed -i '/app\/MSA/d' system/config/system_file_contexts
-	#sudo sed -i '/app\/MSA/d' system/config/system_fs_config
-	#
+	# 搜狗输入法
 	sudo rm -rf system/system/system/app/SougouInput
-	#sudo sed -i '/app\/SougouInput/d' system/config/system_file_contexts
-	#sudo sed -i '/app\/SougouInput/d' system/config/system_fs_config
 	#
 	sudo rm -rf system/system/system/app/Stk
-	#sudo sed -i '/app\/Stk/d' system/config/system_file_contexts
-	#sudo sed -i '/app\/Stk/d' system/config/system_fs_config
-
+	#
 	sudo rm -rf system/system/system/data-app/com.ss.android.article.video_154
-	#sudo sed -i '/data-app\/com\\\.ss\\\.android\\\.article\\\.video_154/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/com\.ss\.android\.article\.video_154/d' system/config/system_fs_config
-
+	#
 	sudo rm -rf system/system/system/data-app/com.ss.android.ugc.aweme_15
-	#sudo sed -i '/data-app\/com\\\.ss\\\.android\\\.ugc\\\.aweme_15/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/com\.ss\.android\.ugc\.aweme_15/d' system/config/system_fs_config
-
+	#
 	sudo rm -rf system/system/system/data-app/com.taobao.taobao_24
-	#sudo sed -i '/data-app\/com\\\.taobao\\\.taobao_24/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/com\.taobao\.taobao_24/d' system/config/system_fs_config
 
 	sudo rm -rf system/system/system/data-app/com.youku.phone_136
-	#sudo sed -i '/data-app\/com\\\.youku\\\.phone_136/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/com\.youku\.phone_136/d' system/config/system_fs_config
 
 	sudo rm -rf system/system/system/data-app/com.zhihu.android_28
-	#sudo sed -i '/data-app\/com\\\.zhihu\\\.android_28/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/com\.zhihu\.android_28/d' system/config/system_fs_config
-
+	# 小米云盘
 	sudo rm -rf system/system/system/data-app/MiDrive
-	#sudo sed -i '/data-app\/MiDrive/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/MiDrive/d' system/config/system_fs_config
-
+	# 锁屏画报
 	sudo rm -rf system/system/system/data-app/MIGalleryLockscreen
-	#sudo sed -i '/data-app\/MIGalleryLockscreen/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/MIGalleryLockscreen/d' system/config/system_fs_config
-
+	# 小米商城
 	sudo rm -rf system/system/system/data-app/MIShop
-	#sudo sed -i '/data-app\/MIShop/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/MIShop/d' system/config/system_fs_config
-
+	# 计算器
 	sudo rm -rf system/system/system/data-app/MIUICalculator
-	#sudo sed -i '/data-app\/MIUICalculator/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/MIUICalculator/d' system/config/system_fs_config
 
 	sudo rm -rf system/system/system/data-app/MIUIDuokanReader
-	#sudo sed -i '/data-app\/MIUIDuokanReader/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/MIUIDuokanReader/d' system/config/system_fs_config
 
 	sudo rm -rf system/system/system/data-app/MIUIGameCenter
-	#sudo sed -i '/data-app\/MIUIGameCenter/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/MIUIGameCenter/d' system/config/system_fs_config
 
 	sudo rm -rf system/system/system/data-app/MIUIHuanji
-	#sudo sed -i '/data-app\/MIUIHuanji/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/MIUIHuanji/d' system/config/system_fs_config
 
 	sudo rm -rf system/system/system/data-app/MIUINewHome
-	#sudo sed -i '/data-app\/MIUINewHome/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/MIUINewHome/d' system/config/system_fs_config
-
+	# 小米有品
 	sudo rm -rf system/system/system/data-app/MIUIYoupin
-	#sudo sed -i '/data-app\/MIUIYoupin/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/MIUIYoupin/d' system/config/system_fs_config
-
+	# 米家
 	sudo rm -rf system/system/system/data-app/SmartHome
-	#sudo sed -i '/data-app\/SmartHome/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/SmartHome/d' system/config/system_fs_config
-
+	# wps-lite
 	sudo rm -rf system/system/system/data-app/wps-lite
-	#sudo sed -i '/data-app\/wps-lite/d' system/config/system_file_contexts
-	#sudo sed -i '/data-app\/wps-lite/d' system/config/system_fs_config
 
 	sudo rm -rf system/system/system/priv-app/MIService
-	#sudo sed -i '/priv-app\/MIService/d' system/config/system_file_contexts
-	#sudo sed -i '/priv-app\/MIService/d' system/config/system_fs_config
-
+	# 浏览器
 	sudo rm -rf system/system/system/priv-app/MIUIBrowser
-	#sudo sed -i '/priv-app\/MIUIBrowser/d' system/config/system_file_contexts
-	#sudo sed -i '/priv-app\/MIUIBrowser/d' system/config/system_fs_config
-
 	# 传送门
-	#sudo rm -rf system/system/system/priv-app/MIUIContentExtension
-	#sudo sed -i '/priv-app\/MIUIContentExtension/d' system/config/system_file_contexts
-	#sudo sed -i '/priv-app\/MIUIContentExtension/d' system/config/system_fs_config
-
+	sudo rm -rf system/system/system/priv-app/MIUIContentExtension
 	# 搜索
-	#sudo rm -rf system/system/system/priv-app/MIUIQuickSearchBox
-	#sudo sed -i '/priv-app\/MIUIQuickSearchBox/d' system/config/system_file_contexts
-	#sudo sed -i '/priv-app\/MIUIQuickSearchBox/d' system/config/system_fs_config
-
+	sudo rm -rf system/system/system/priv-app/MIUIQuickSearchBox
 	# 音乐
 	# sudo rm -rf system/system/system/priv-app/Music
-	# sed -i '/priv-app/Music/d' system/config/system_file_contexts
-	# sed -i '/priv-app/Music/d' system/config/system_fs_config
-
-
-	# system_ext
-	# sudo sed -i '0,/[a-z]\+\/lost\\+found/{/[a-z]\+\/lost\\+found/d}' system_ext/config/system_ext_file_contexts
-
 
 	# vendor
-	#sudo sed -i '0,/[a-z]\+\/lost\\+found/{/[a-z]\+\/lost\\+found/d}' vendor/config/vendor_file_contexts
+	sudo sed -i '0,/[a-z]\+\/lost\\+found/{/[a-z]\+\/lost\\+found/d}' vendor/config/vendor_file_contexts
 
 	# 去除 AVB
-	#sudo sed -i 's/,avb//g' vendor/vendor/etc/fstab.qcom
-	#sudo sed -i 's/,avb=vbmeta_system//g' vendor/vendor/etc/fstab.qcom
-	#sudo sed -i 's/,avb_keys=\/avb\/q-gsi.avbpubkey:\/avb\/r-gsi.avbpubkey:\/avb\/s-gsi.avbpubkey//g' vendor/vendor/etc/fstab.qcom
+	sudo sed -i 's/,avb//g' vendor/vendor/etc/fstab.qcom
+	sudo sed -i 's/,avb=vbmeta_system//g' vendor/vendor/etc/fstab.qcom
+	sudo sed -i 's/,avb_keys=\/avb\/q-gsi.avbpubkey:\/avb\/r-gsi.avbpubkey:\/avb\/s-gsi.avbpubkey//g' vendor/vendor/etc/fstab.qcom
 
 
 	# product
-	#sudo sed -i '0,/[a-z]\+\/lost\\+fou#nd/{/[a-z]\+\/lost\\+found/d}' product/config/product_file_contexts
-
-	#sudo rm -rf product/product/data-app/BaiduIME
-	#sudo sed -i '/data-app\/BaiduIME/d' product/config/product_file_contexts
-	#sudo sed -i '/data-app\/BaiduIME/d' product/config/product_fs_config
+	sudo sed -i '0,/[a-z]\+\/lost\\+fou#nd/{/[a-z]\+\/lost\\+found/d}' product/config/product_file_contexts
+	# 百度输入法
+	sudo rm -rf product/product/data-app/BaiduIME
 
 	# DC调光
-	#sudo sed -i 's/<bool name=\"support_dc_backlight\">false<\/bool>/<bool name=\"support_dc_backlight\">true<\/bool>/g' product/product/etc/device_features/*xml
-	#sudo sed -i 's/<bool name=\"support_secret_dc_backlight\">true<\/bool>/<bool name=\"support_secret_dc_backlight\">false<\/bool>/g' product/product/etc/device_features/*xml
+	sudo sed -i 's/<bool name=\"support_dc_backlight\">false<\/bool>/<bool name=\"support_dc_backlight\">true<\/bool>/g' product/product/etc/device_features/*xml
+	sudo sed -i 's/<bool name=\"support_secret_dc_backlight\">true<\/bool>/<bool name=\"support_secret_dc_backlight\">false<\/bool>/g' product/product/etc/device_features/*xml
 
 	# 智能护眼
 	# sudo sed -i '/<\/features>/i\    <bool name=\"support_smart_eyecare\">true<\/bool>' product/product/etc/device_features/*xml
@@ -214,7 +209,6 @@ function modify(){
 
 	# 游戏英雄死亡倒计时
 	#sudo sed -i '/<\/features>/i\    <bool name=\"support_mi_game_countdown\">true<\/bool>' product/product/etc/device_features/*xml
-
 
 }
 
